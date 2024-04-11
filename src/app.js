d8bb97b92e9b51b76642509376385ccf64b0de1b
matischlegel <161637922+matischlegel@users.noreply.github.com>
2024-04-11 22:19:23 +0000

    termine el proyecto

--- src/app.js
+++ src/app.js
@@ -1,11 +1,25 @@
 /* eslint-disable */
-import "bootstrap";
-import "./style.css";
+//import "bootstrap";
+//import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+//import "./assets/img/rigo-baby.jpg";
+//import "./assets/img/4geeks.ico";
 
-window.onload = function() {
+// window.onload = function generateNames() {
   //write your code here
-  console.log("Hello Rigo from the console!");
-};
+  let pronoun = ["el", "nuestro", "mi", "su", "tu"];
+  let adj = ["buen", "gran", "pequeño", "hermoso"];
+  let noun = ["corredor", "mapache", "ciudadano", "perro", "gato"];
+
+  let containerNames = [];
+
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let q = 0; q < adj.length; q++) {
+      for (let k = 0; k < noun.length; k++) {
+        let result = pronoun[i] + adj[q] + noun[k] + ".com";
+        console.log(result);
+      }
+    }
+  }
+// };
+// generateNames();
